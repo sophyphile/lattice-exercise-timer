b39a0db2e816059f694fb6c93d52b3c883b57520
sophyphile <67548331+sophyphile@users.noreply.github.com>
2025-08-25 16:04:28 +0100

    Updates +not-found.tsx and installs web libraries to test unmatched routes

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,27 +1,40 @@
-import { Link, Stack } from 'expo-router'
-import { StyleSheet, View } from 'react-native'
+import { router, Stack } from "expo-router";
+import { Pressable, StyleSheet, View, Text } from "react-native";
 
 export default function NotFoundScreen() {
   return (
     <>
-        <Stack.Screen options={{ title: 'Oops! Not Found' }} />
-        <View style={styles.container}>
-            <Link href="/" style={styles.button}>Go back to Home Screen!</Link>
-        </View>
+      <Stack.Screen options={{ title: "Oops! Not Found" }} />
+      <View style={styles.container}>
+        <Pressable style={styles.button} onPress={() => router.replace("/")}>
+          <Text style={styles.buttonText}>
+            Oops! Page not found. Go back to the Home Screen!
+          </Text>
+        </Pressable>
+      </View>
     </>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: '#fff',
-    }
-})
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    padding: 40,
+    backgroundColor: "#121212",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#307eae",
+    paddingVertical: 14,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#ffffff",
+    fontWeight: "bold",
+  },
+});
