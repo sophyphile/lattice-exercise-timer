0ca05a6289a5316f1e3528a6ae68a61948c3f525
sophyphile <67548331+sophyphile@users.noreply.github.com>
2025-08-25 11:16:30 +0100

    Makes dependency changes and adds +not-found.tsx file

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
         <Stack
           screenOptions={{
             headerShown: true,
+            headerShadowVisible: false,
             headerTitle: () => (
               <View style={{ flexDirection: "row", alignItems: "center" }}>
                 <LatticeLogo
